006f575f4f63eae9230b08bf1736fd72402fc052
CarlosCardosoM <carlos.cardoso@ufvjm.edu.br>
2025-06-08 13:35:59 -0300

    .
    .

--- mobile/App.js
+++ mobile/App.js
@@ -1,21 +1,25 @@
+import { registerRootComponent } from 'expo'; // <-- 1. IMPORTAR O REGISTRADOR
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './screens/LoginScreen'; // Vamos criar essa e outras
+import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
 import CadastroScreen from './screens/CadastroScreen';
 
 const Stack = createStackNavigator();
 
-export default function App() {
+// Remova o "export default" daqui
+function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Cadastro" component={CadastroScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
-        {/* Outras telas */}
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+// 2. REGISTRAR O COMPONENTE 'APP' COMO O PRINCIPAL ('main')
+registerRootComponent(App);
